bae790a0bb0647ca53b23d533c90b876d851f759
Sarath KH <sarathhaldurai@gmail.com>
2024-08-12 18:41:59 +0530

    Add files via upload

---
+++ Task1/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import '../src/App.css';
+// import './style.css';
+// import Personaldetails from "./Components/About";
+
+// import Task3 from './Components/Task3';
+import Task1 from './Components/Task1';
+
+function App() {
+  return (
+    
+   
+    <>
+    <Task1/>
+    
+    </>
+  );
+}
+
+export default App;
